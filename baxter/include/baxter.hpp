a0b9438fce99be060fa54253806b7a70bc77350a
YoshimitsuMatsutaIe <matsuta2718@gmail.com>
2022-05-08 00:08:13 +0900

    baxter compile error

--- baxter/include/baxter.hpp
+++ baxter/include/baxter.hpp
@@ -4,7 +4,7 @@
 #include<eigen3/Eigen/Core>
 
 #include <vector>
-
+#include <array>
 #include "../../include/mappings.hpp"
 
 namespace baxter
@@ -12,27 +12,42 @@ namespace baxter
     using std::vector;
     using Eigen::VectorXd;
     using Eigen::MatrixXd;
-    static const double L = 278e-3;
-    static const double h = 64e-3;
-    static const double H = 1104e-3;
-    static const double L0 = 270.35e-3;
-    static const double L1 = 69e-3;
-    static const double L2 = 364.35e-3;
-    static const double L3 = 69e-3;
-    static const double L4 = 374.29e-3;
-    static const double L5 = 10e-3;
-    static const double L6 = 368.3e-3;
-    static const double R = 0.05;  // 腕の太さの基準．不明
+    // const double L;
+    // const double h;
+    // const double H;
+    // const double L0;
+    // const double L1;
+    // const double L2;
+    // const double L3;
+    // const double L4;
+    // const double L5;
+    // const double L6;
+    // const double R;
+    // const VectorXd Q_NEUTRAL(7);
+    // const VectorXd Q_MAX(7);
+    // const VectorXd Q_MIN(7);
 
-    vector<double> Q_NEUTRAL = {
-        0.0, -31.0*M_PI/180, 0.0, 43.0*M_PI/180, 0.0, 72.0*M_PI/180, 0.0
-    };
-    vector<double> Q_MAX = {
-        51.0*M_PI/180, 60.0*M_PI/180, 173.0*M_PI/180, 150.0*M_PI/180, 175.0*M_PI/180, 120.0*M_PI/180, 175.0*M_PI/180
-    };
-    vector<double> Q_MIN = {
-        -141.0*M_PI/180, -123.0*M_PI/180, -173.0*M_PI/180, -3.0*M_PI/180, -175.0*M_PI/180, -90.0*M_PI/180, -175.0*M_PI/180
-    };
+
+    const double L = 278e-3;
+    const double h = 64e-3;
+    const double H = 1104e-3;
+    const double L0 = 270.35e-3;
+    const double L1 = 69e-3;
+    const double L2 = 364.35e-3;
+    const double L3 = 69e-3;
+    const double L4 = 374.29e-3;
+    const double L5 = 10e-3;
+    const double L6 = 368.3e-3;
+    const double R = 0.05;
+
+
+
+
+
+
+
+
+
 
 
     vector<vector<double>> r_bars_0{
@@ -83,7 +98,7 @@ namespace baxter
         {0, 0, 0, 1}
     };
 
-    static vector<vector<vector<double>>> R_BARS_ALL{
+    vector<vector<vector<double>>> R_BARS_ALL{
         r_bars_0, r_bars_1, r_bars_2, r_bars_3, r_bars_4, r_bars_5, r_bars_6, r_bars_ee
     };
 
--- baxter/src/baxter.cpp
+++ baxter/src/baxter.cpp
@@ -1,9 +1,55 @@
 #include "../include/baxter.hpp"
 
 
+
+// const double baxter::L = 278e-3;
+// const double baxter::h = 64e-3;
+// const double baxter::H = 1104e-3;
+// const double baxter::L0 = 270.35e-3;
+// const double baxter::L1 = 69e-3;
+// const double baxter::L2 = 364.35e-3;
+// const double baxter::L3 = 69e-3;
+// const double baxter::L4 = 374.29e-3;
+// const double baxter::L5 = 10e-3;
+// const double baxter::L6 = 368.3e-3;
+// const double baxter::R = 0.05;
+
+
+// const Eigen::VectorXd baxter::Q_NEUTRAL(
+//     0.0,
+//     -31.0*M_PI/180,
+//     0.0,
+//     43.0*M_PI/180,
+//     0.0,
+//     72.0*M_PI/180,
+//     0.0
+// );
+// const Eigen::VectorXd baxter::Q_MAX(
+//     51.0*M_PI/180,
+//     60.0*M_PI/180,
+//     173.0*M_PI/180,
+//     150.0*M_PI/180,
+//     175.0*M_PI/180,
+//     120.0*M_PI/180,
+//     175.0*M_PI/180
+// );
+// const Eigen::VectorXd baxter::Q_NEUTRAL(
+//     -141.0*M_PI/180,
+//     -123.0*M_PI/180,
+//     -173.0*M_PI/180,
+//     -3.0*M_PI/180,
+//     -175.0*M_PI/180,
+//     -90.0*M_PI/180,
+//     -175.0*M_PI/180
+// );
+
+
+
+
 baxter::Control_Point::Control_Point(int frame, int index)
 {
-    //this->r_bar = Eigen::Map<VectorXd>(R_BARS_ALL[frame][index].data());
+    this->r_bar = VectorXd::Zero(4);
+    this->r_bar << R_BARS_ALL[frame][index][0], R_BARS_ALL[frame][index][1], R_BARS_ALL[frame][index][2], 1.0;
     this->calc_htm = HTMs[frame];
     this->calc_jo = JOs[frame];
     this->calc_jrx = JRXs[frame];
