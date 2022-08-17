02079a25111a981c199bbc085c7830cb9a3235f3
YoshimitsuMatsutaIe <matsuta2718@gmail.com>
2022-08-17 15:35:08 +0900

    sphere, cylinder etc

--- include/environment.hpp
+++ include/environment.hpp
@@ -16,7 +16,16 @@ namespace simulator
     using Eigen::MatrixXd;
     void rotate(float alpha, float beta, float gamma, Eigen::Matrix3d& out);
     void set_sphere(int n, float r, float x, float y, float z, vector<VectorXd>& out);
-
+    void set_cylinder(
+        int n, float r, float L, float x, float y, float z,
+        float alpha, float beta, float gamma,
+        vector<VectorXd>& out
+    );
+    void set_field(
+        int n, float lx, float ly, float x, float y, float z,
+        float alpha, float beta, float gamma,
+        vector<VectorXd>& out
+    );
 
 
 
--- src/environment.cpp
+++ src/environment.cpp
@@ -1,7 +1,7 @@
 #include "../include/environment.hpp"
 
 /**
- * @brief 3次元回転行列
+ * @brief 3次元回転行列（度で指定）
  * 
  * @param alpha 
  * @param beta 
@@ -13,6 +13,10 @@ void simulator::rotate(float alpha, float beta, float gamma, Eigen::Matrix3d& ou
     Eigen::Matrix3d Rx;
     Eigen::Matrix3d Ry;
     Eigen::Matrix3d Rz;
+
+    alpha = alpha * M_PI / 180.0;
+    beta = beta * M_PI / 180.0;
+    gamma = gamma * M_PI / 180.0;
 
     Rx << 1, 0, 0,
     0, cos(alpha), -sin(alpha),
@@ -28,6 +32,7 @@ void simulator::rotate(float alpha, float beta, float gamma, Eigen::Matrix3d& ou
 
     out = Rx * Ry * Rz;
 }
+
 
 
 void simulator::set_sphere(int n, float r, float x, float y, float z, std::vector<Eigen::VectorXd>& out)
@@ -55,4 +60,72 @@ void simulator::set_sphere(int n, float r, float x, float y, float z, std::vecto
 
     //std::cout << "n = " << n << std::endl;
     //std::cout << "sp done!" << std::endl;
+}
+
+
+
+void simulator::set_cylinder(
+    int n, float r, float L, float x, float y, float z,
+    float alpha, float beta, float gamma,
+    vector<VectorXd>& out
+)
+{
+    std::random_device seed_gen;
+    std::default_random_engine engine(seed_gen());
+    std::uniform_real_distribution<> dist_theta(0, 2*M_PI);
+    std::uniform_real_distribution<> dist_l(-L*0.5, L*0.5);
+
+    Eigen::Matrix3d R;
+    rotate(alpha, beta, gamma, R);
+
+    VectorXd center(3);
+    center << x, y, z;
+
+    float theta;
+    float l;
+
+    for (int i=0; i<n; i++){
+        theta = dist_theta(engine);
+        l = dist_l(engine);
+
+        VectorXd o(3);
+        o << r * cos(theta),
+        r * sin(theta),
+        l;
+
+        out.push_back(R * o + center);
+    }
+}
+
+
+
+void simulator::set_field(
+    int n, float lx, float ly, float x, float y, float z,
+    float alpha, float beta, float gamma,
+    vector<VectorXd>& out
+)
+{
+    std::random_device seed_gen;
+    std::default_random_engine engine(seed_gen());
+    std::uniform_real_distribution<> dist_x_(-lx*0.5, lx*0.5);
+    std::uniform_real_distribution<> dist_y_(-ly*0.5, ly*0.5);
+
+    Eigen::Matrix3d R;
+    rotate(alpha, beta, gamma, R);
+
+    VectorXd center(3);
+    center << x, y, z;
+
+    float x_;
+    float y_;
+
+    for (int i=0; i<n; i++){
+        x_ = dist_x_(engine);
+        y_ = dist_y_(engine);
+
+        VectorXd o(3);
+        o << x_, y_, 0;
+
+        out.push_back(R * o + center);
+    }
 }
